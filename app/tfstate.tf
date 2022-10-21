dd4f9287495d1dd9b09a0e43ce9fdd3f3545a914
Pablo Sánchez Alonso <pablo.sanchez@codurance.com>
2022-10-21 10:32:00 +0100

    Added makefile and other things

--- app/tfstate.tf
+++ app/tfstate.tf
@@ -3,9 +3,9 @@ data "terraform_remote_state" "jrs" {
     backend = "azurerm"
 
     config = {
-        resource_group_name  = "dev-watson-rg-jenkins-tfstate"
-        storage_account_name = "devwatsontfstatesa"
-        container_name       = "tfstate"
+        resource_group_name  = "dev-watson-infrastate-rg"
+        storage_account_name = "devwatsoninfrastatesa"
+        container_name       = "dev-watson-jenkins-infrastate"
         key                  = "terraform.tfstate"
     }
 }
--- app/versions.tf
+++ app/versions.tf
@@ -7,4 +7,11 @@ terraform {
   }
 
   required_version = ">= 1.3.0"
+
+  backend "azurerm" {
+      resource_group_name  = ""
+      storage_account_name = ""
+      container_name       = ""
+      key                  = "terraform.tfstate"
+  }
 }
