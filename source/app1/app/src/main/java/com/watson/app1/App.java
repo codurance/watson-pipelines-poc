0b3bf47677a0a504e023c6e138d1ed9bf6876758
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2022-10-24 14:18:48 +0100

    Add endpoints for getting employees and employees by ID with Spark

--- source/app1/app/src/main/java/com/watson/app1/App.java
+++ source/app1/app/src/main/java/com/watson/app1/App.java
@@ -1,12 +1,53 @@
 package com.watson.app1;
 
+import static spark.Spark.get;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+import com.watson.app1.employees.get.GetEmployee;
+import com.watson.app1.employees.get.GetEmployeeInput;
+import com.watson.app1.employees.getall.GetAllEmployees;
+import com.watson.app1.employees.getall.GetAllEmployeesInput;
+import java.util.List;
+import spark.Request;
+import spark.Response;
+
 public class App {
 
-  public String getGreeting() {
-    return "Hello World!";
+  public static void main(String[] args) {
+    App app = new App();
+
+    EmployeesRepository repository = new InMemoryEmployeeRepository();
+    GetAllEmployeesInput getAllEmployeesInput = new GetAllEmployees(repository);
+    GetEmployeeInput getEmployeeInput = new GetEmployee(repository);
+    EmployeeParser parser = new EmployeeParser();
+    EmployeesService service = new EmployeesService(getAllEmployeesInput, getEmployeeInput,
+        parser);
+
+    Moshi moshi = new Moshi.Builder().build();
+
+    get("/employees", (req, res) -> app.getEmployees(res, moshi, service));
+    get("/employees/:id", (req, res) -> app.getEmployeesById(req, res, moshi, service));
   }
 
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
+  private Object getEmployees(Response res, Moshi moshi,
+      EmployeesService service) {
+    res.header("Content-Type", "application/json");
+    JsonAdapter<List<Employee>> employeesListAdapter = moshi.adapter(
+        Types.newParameterizedType(List.class, Employee.class));
+    return employeesListAdapter.toJson(service.getAll());
+  }
+
+  private Object getEmployeesById(Request req, Response res, Moshi moshi,
+      EmployeesService service) {
+    try {
+      res.header("Content-Type", "application/json");
+      JsonAdapter<Employee> employeeAdapter = moshi.adapter(Employee.class);
+      return employeeAdapter.toJson(service.getEmployee(Integer.parseInt(req.params(":id"))));
+    } catch (EmployeeNotAvailable e) {
+      res.status(404);
+      return e.getCause().getMessage();
+    }
   }
 }
