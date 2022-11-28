c86c7285edc5901828e83bfa84ca35fb27735c60
Edwy MANDRET <emandret@student.42.fr>
2022-11-28 17:55:26 +0000

    Updated tf config

--- infra/main/outputs.tf
+++ infra/main/outputs.tf
@@ -1,8 +1,3 @@
-output "public_ip_address" {
-  description = "The Primary Public IP Address assigned to this Virtual Machine."
-  value       = azurerm_linux_virtual_machine.vm.public_ip_address
-}
-
 output "resource_group_location" {
   description = "The Resource Group Location."
   value       = azurerm_resource_group.rg.location
@@ -11,11 +6,6 @@ output "resource_group_location" {
 output "resource_group_name" {
   description = "The Resource Group Name."
   value       = azurerm_resource_group.rg.name
-}
-
-output "service_plan_id" {
-  description = "The ID of the App Service plan."
-  value       = azurerm_service_plan.sp.id
 }
 
 output "container_registry_login_server" {
@@ -32,19 +22,4 @@ output "container_registry_admin_password" {
   description = "The Container Registry admin password."
   value       = azurerm_container_registry.cr.admin_password
   sensitive   = true
-}
-
-output "integration_subnet_id" {
-  description = "The integration subnet ID."
-  value       = azurerm_subnet.integration.id
-}
-
-output "endpoint_subnet_id" {
-  description = "The endpoint subnet ID."
-  value       = azurerm_subnet.endpoint.id
-}
-
-output "private_dns_zone_id" {
-  description = "The private DNS zone ID."
-  value       = azurerm_private_dns_zone.dnsprivatezone.id
 }
