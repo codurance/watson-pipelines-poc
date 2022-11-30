20125558195a73a88f4417733f11b1c3cf3f364a
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2022-11-30 15:16:27 +0000

    Comment out the payroll app service and get the main infra vars from the tfvars

--- infra/app/locals.tf
+++ infra/app/locals.tf
@@ -1,12 +1,12 @@
 locals {
   prefix                                 = "${var.environment}-${var.project}"
-  main_resource_group_location           = data.terraform_remote_state.trs.outputs.resource_group_location
-  main_resource_group_name               = data.terraform_remote_state.trs.outputs.resource_group_name
-  main_service_plan_id                   = data.terraform_remote_state.trs.outputs.service_plan_id
-  main_container_registry_login_server   = data.terraform_remote_state.trs.outputs.container_registry_login_server
-  main_container_registry_admin_username = data.terraform_remote_state.trs.outputs.container_registry_admin_username
-  main_container_registry_admin_password = data.terraform_remote_state.trs.outputs.container_registry_admin_password
-  main_integration_subnet_id             = data.terraform_remote_state.trs.outputs.integration_subnet_id
-  main_endpoint_subnet_id                = data.terraform_remote_state.trs.outputs.endpoint_subnet_id
-  main_private_dns_zone_id               = data.terraform_remote_state.trs.outputs.private_dns_zone_id
+  main_resource_group_name               = var.resource_group_name
+  main_resource_group_location           = var.resource_group_location
+  main_service_plan_id                   = var.service_plan_id
+  main_container_registry_login_server   = var.container_registry_login_server
+  main_container_registry_admin_username = var.container_registry_admin_username
+  main_container_registry_admin_password = var.container_registry_admin_password
+  main_integration_subnet_id             = var.integration_subnet_id
+  main_endpoint_subnet_id                = var.endpoint_subnet_id
+  main_private_dns_zone_id               = var.private_dns_zone_id
 }
--- infra/app/variables.tf
+++ infra/app/variables.tf
@@ -10,47 +10,47 @@ variable "project" {
 
 variable "subscription_id" {
   type        = string
-  description = "The Azure account subscribtion id."
+  description = "The Azure account subscription id."
 }
 
-variable "main_remote_state_resource_group_name" {
+variable "client_id" {
   type        = string
-  description = "The resource group name where the remote state storage account lives."
+  description = "The Azure Service Principal client id."
 }
 
-variable "main_remote_state_storage_account_name" {
+variable "client_secret" {
   type        = string
-  description = "The storage account name of the remote state."
+  description = "The Azure Service Principal client secret."
 }
 
-variable "main_remote_state_container_name" {
+variable "tenant_id" {
   type        = string
-  description = "The container name where the remote state lives."
+  description = "The Azure Service Principal tenant id."
 }
 
-variable "employees_url_protocol" {
-  type        = string
-  description = "The protocol used to connect to the Employees service."
-  default     = "http"
-}
+# variable "employees_url_protocol" {
+#   type        = string
+#   description = "The protocol used to connect to the Employees service."
+#   default     = "http"
+# }
 
-variable "employees_url_port" {
-  type        = string
-  description = "The port used to connect to the Employees service."
-  default     = "80"
-}
+# variable "employees_url_port" {
+#   type        = string
+#   description = "The port used to connect to the Employees service."
+#   default     = "80"
+# }
 
-variable "payroll_docker_image_name" {
-  type        = string
-  description = "The Payroll service container name."
-  default     = "payroll"
-}
+# variable "payroll_docker_image_name" {
+#   type        = string
+#   description = "The Payroll service container name."
+#   default     = "payroll"
+# }
 
-variable "payroll_docker_image_tag" {
-  type        = string
-  description = "The Payroll service container tag."
-  default     = "latest"
-}
+# variable "payroll_docker_image_tag" {
+#   type        = string
+#   description = "The Payroll service container tag."
+#   default     = "latest"
+# }
 
 variable "employees_docker_image_name" {
   type        = string
@@ -62,4 +62,69 @@ variable "employees_docker_image_tag" {
   type        = string
   description = "The Employees service container tag."
   default     = "latest"
+}
+
+##########################################
+# We will later get the below variables
+# from the remote state of the main infra
+##########################################
+
+# variable "main_remote_state_resource_group_name" {
+#   type        = string
+#   description = "The resource group name where the remote state storage account lives."
+# }
+
+# variable "main_remote_state_storage_account_name" {
+#   type        = string
+#   description = "The storage account name of the remote state."
+# }
+
+# variable "main_remote_state_container_name" {
+#   type        = string
+#   description = "The container name where the remote state lives."
+# }
+
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group name."
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "The resource group location."
+}
+
+variable "service_plan_id" {
+  type        = string
+  description = "The service plan ID from the main infra."
+}
+
+variable "container_registry_login_server" {
+  type        = string
+  description = "The container registry login server from the main infra."
+}
+
+variable "container_registry_admin_username" {
+  type        = string
+  description = "The container registry admin username from the main infra."
+}
+
+variable "container_registry_admin_password" {
+  type        = string
+  description = "The container registry admin password from the main infra."
+}
+
+variable "integration_subnet_id" {
+  type        = string
+  description = "The integration subnet id from the main infra."
+}
+
+variable "endpoint_subnet_id" {
+  type        = string
+  description = "The endpoint subnet id from the main infra."
+}
+
+variable "private_dns_zone_id" {
+  type        = string
+  description = "The the private dns zone id from the main infra."
 }
